b89fe89648e94a6efc9da966069f16cd2ae9e51e
shirin <shirin.hdi8@gmail.com>
2025-01-22 10:55:37 +0200

    Kokonaisluku

---
+++ 02-4-Kokonaisluku.py
@@ -0,0 +1,11 @@
+Ensimmäinen_luku = int(input("Anna ensimmäinen kokonaisluku: "))
+Toinen_luku = int(input("Anna toinen luku: "))
+Kolmas_luku = int(input("Anna kolmas luku: "))
+
+Lukujen_summa = Ensimmäinen_luku + Toinen_luku + Kolmas_luku
+Lukujen_tulo = Ensimmäinen_luku * Toinen_luku * Kolmas_luku
+Lukujen_keskiarvo = Lukujen_summa / 3
+
+print(f"Lukujen summa on {Lukujen_summa}")
+print(f"Lukujen tulo on {Lukujen_tulo}")
+print(f"Lukujen keskiarvo on {Lukujen_keskiarvo:.2f}")
