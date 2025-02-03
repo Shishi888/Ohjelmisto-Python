107927a173f608c80cda5986b6aa33520e6a9d66
Shishi888 <122361934+Shishi888@users.noreply.github.com>
2025-02-03 18:16:03 +0200

    Alkuluku

---
+++ 05-3-Alkuluku.py
@@ -0,0 +1,15 @@
+luku = int(input("Anna kokonaisluku: "))
+
+if luku < 2:
+    print(f"{luku} ei ole alkuluku.")
+else:
+    alkuluku = True
+    for i in range(2, int(luku ** 0.5) + 1):
+        if luku % i == 0:
+            alkuluku = False
+            break
+
+    if alkuluku:
+        print(f"{luku} on alkuluku.")
+    else:
+        print(f"{luku} ei ole alkuluku.")
