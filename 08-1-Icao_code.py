b31acc7e41a8f2ccae99b28f426ac90ce7acc062
Shishi888 <122361934+Shishi888@users.noreply.github.com>
2025-03-15 21:25:10 +0200

    Lentoasematieto

---
+++ 08-1-Icao_code.py
@@ -0,0 +1,39 @@
+import mysql.connector
+
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='RIRI@eshgh2020',
+    autocommit=True
+)
+
+def hae_lentokenttä(icao_koodi):
+    try:
+
+        kursori = yhteys.cursor()
+        sql = f"SELECT name, municipality FROM airport WHERE ident = '{icao_koodi}'"
+        kursori.execute(sql)
+
+        tulos = kursori.fetchone()
+
+        if tulos:
+            kentän_nimi, sijaintikunta = tulos
+            print(f"Lentokentän nimi: {kentän_nimi}")
+            print(f"Sijaintikunta: {sijaintikunta}")
+        else:
+            print("Kenttää ei löytynyt tällä ICAO-koodilla.")
+
+    except mysql.connector.Error as e:
+        print(f"Tapahtui virhe: {e}")
+    finally:
+
+        kursori.close()
+
+
+icao_koodi = input("Syötä ICAO-koodi: ")
+
+
+hae_lentokenttä(icao_koodi)
